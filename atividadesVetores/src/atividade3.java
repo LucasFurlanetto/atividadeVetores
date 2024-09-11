cc6154b9b38e97b51b9677e017c23ade89633c07
LucasFurlanetto <127804969+LucasFurlanetto@users.noreply.github.com>
2024-09-10 23:33:42 -0300

    Add files via upload

---
+++ atividadesVetores/src/atividade3.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class atividade3 {
+    public static void main(String[] args) {
+
+        Integer[] valores = {5, 10};
+
+        valores[0] = 5;
+        valores[1] = 10;
+
+        Arrays.sort(valores, Collections.reverseOrder());
+
+        System.out.println(Arrays.toString(valores));
+
+
+    }
+}
